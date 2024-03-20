aba53c8466d92d0f9409a41227169f70bb2a0ca8
Dwayne67E <elias@enkirche.fr>
2024-03-20 17:52:08 +0100

    john

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+module.exports = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false, // Ignore le module `fs` lors du bundling
+    };
+
+    return config;
+  },
+};
